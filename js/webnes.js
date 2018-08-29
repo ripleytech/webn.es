1bebfa5f0ccfc36e7c301caeb279ff71665e5c49
Ripley <ripleytech@gmail.com>
2018-08-28 21:12:55 -0700

    Add files via upload

--- js/webnes.js
+++ js/webnes.js
@@ -157,11 +157,8 @@ $(function() {
         $('#scroll ul').append(renderItem(result.rows.item(i)));
       }
       if (result.rows.length == 0) {
-        addRom('Croom', 'roms/croom.nes');
-        addRom('Tetramino', 'roms/lj65.nes');
-        addRom('Galaxy Patrol', 'roms/galaxy.nes');
-        addRom('Fighter F-8000', 'roms/fighter_f8000.nes');
-        addRom('BoxBoy', 'roms/BOXBOY.nes');
+        addRom('Super Maxine Wtrs.', 'roms/smw.nes');
+     
       }
     });
   });  
